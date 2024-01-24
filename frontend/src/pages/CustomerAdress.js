c51f8ec8816fee7af72157b9264b9ecffaa4a4ae
srinathmacharla <153605611+srinathmacharla@users.noreply.github.com>
2024-01-24 18:46:00 +0530

    Update CustomerAdress.js

--- frontend/src/pages/CustomerAdress.js
+++ frontend/src/pages/CustomerAdress.js
@@ -87,13 +87,13 @@ const CustomerAdress = () => {
     };
     var rzp1 = new window.Razorpay(options);
     rzp1.on("payment.failed", function (response) {
-      alert(response.error.code);
-      alert(response.error.description);
-      alert(response.error.source);
-      alert(response.error.step);
+      // alert(response.error.code);
+      // alert(response.error.description);
+      // alert(response.error.source);
+      // alert(response.error.step);
       alert(response.error.reason);
-      alert(response.error.metadata.order_id);
-      alert(response.error.metadata.payment_id);
+      // alert(response.error.metadata.order_id);
+      // alert(response.error.metadata.payment_id);
     });
     rzp1.open();
     e.preventDefault();
